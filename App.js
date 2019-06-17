1bc906fc27fe75d7c44f1dea2bf562075d7a43fa
root <g.r.almeida@live.com>
2019-06-17 20:21:46 -0300

    Creating the Header

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './src/components/Header';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
+    <View>
+      <Header />
     </View>
   );
 }
