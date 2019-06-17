394b3d56a73589a0c9a873121bc11a229404114b
root <g.r.almeida@live.com>
2019-06-17 20:54:46 -0300

    Rendering a list

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './src/components/Header';
 
-export default function App() {
-  return (
-    <View>
-      <Header title='People' />
-    </View>
-  );
+export default class App extends Component {
+  renderList () {
+    const names = [
+      'Steve Vai',
+      'Joe Satriani',
+      'Eddie Van Halen',
+      'Jimi Hendrix'
+    ];
+
+    const textElements = names.map((name, index) => {
+      return <Text key={index}>{name}</Text>
+    });
+
+    return textElements;
+  }
+
+  render () {
+    return (
+      <View>
+        <Header title='People' />
+        {this.renderList()}
+      </View>
+    );
+  }
 }
