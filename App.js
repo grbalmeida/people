5b6ec4e2ad11d141d1d25a2ef2611d5c56554df4
root <g.r.almeida@live.com>
2019-06-17 21:09:22 -0300

    Searching for API data

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 import Header from './src/components/Header';
 
 export default class App extends Component {
   renderList () {
-    const names = [
-      'Steve Vai',
-      'Joe Satriani',
-      'Eddie Van Halen',
-      'Jimi Hendrix'
-    ];
-
-    const textElements = names.map((name, index) => {
-      return <Text key={index}>{name}</Text>
-    });
-
-    return textElements;
+    axios
+      .get('https://randomuser.me/api/?nat=br&results=5')
+      .then(response => {
+        const { results } = response.data;
+        const names = results.map(person => person.name.first);
+        console.log(names);
+      });
   }
 
   render () {
