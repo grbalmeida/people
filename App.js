4e9431515ef08fc4b17d2d2a8d537f4a52aa4dfd
root <g.r.almeida@live.com>
2019-06-19 19:49:31 -0300

    Adding a new Header

--- App.js
+++ App.js
@@ -6,6 +6,21 @@ const StackNavigator = createStackNavigator({
   'Main': {
     screen: PeoplePage
   }
+}, {
+  defaultNavigationOptions: {
+    title: 'Pessoas',
+    headerStyle: {
+      backgroundColor: '#6ca2f7',
+      borderBottomWidth: 1,
+      borderBottomColor: '#c5c5c5'
+    },
+    headerTitleStyle: {
+      color: 'white',
+      fontSize: 30,
+      flexGrow: 1,
+      textAlign: 'center'
+    }
+  }
 })
 
 const AppContainer = createAppContainer(StackNavigator)
--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import axios from 'axios'
 
-import Header from '../components/Header'
 import PeopleList from '../components/PeopleList'
 
 export default class App extends Component {
@@ -26,7 +25,6 @@ export default class App extends Component {
   render () {
     return (
       <View>
-        <Header title='People' />
         <PeopleList people={this.state.people} />
       </View>
     )
