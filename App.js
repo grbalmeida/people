65fe54d00bdb7e277404078f86d3a624373ce999
root <g.r.almeida@live.com>
2019-06-19 04:50:12 -0300

    Preparing for next pages

--- App.js
+++ App.js
@@ -1,34 +1,3 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import axios from 'axios'
+import PeoplePage from './src/pages/PeoplePage'
 
-import Header from './src/components/Header'
-import PeopleList from './src/components/PeopleList'
-
-export default class App extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      people: []
-    }
-  }
-
-  componentDidMount () {
-    axios
-      .get('https://randomuser.me/api/?nat=br&results=5')
-      .then(response => {
-        const { results } = response.data
-        this.setState({ people: results })
-      })
-  }
-
-  render () {
-    return (
-      <View>
-        <Header title='People' />
-        <PeopleList people={this.state.people} />
-      </View>
-    )
-  }
-}
+export default PeoplePage
