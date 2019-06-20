e2e76c1ad41a94c7244144f135d8391b81bc3653
root <g.r.almeida@live.com>
2019-06-20 18:35:38 -0300

    Adding a loading with ActivityIndicator

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, ActivityIndicator } from 'react-native'
 import axios from 'axios'
 
 import PeopleList from '../components/PeopleList'
@@ -9,22 +9,33 @@ export default class App extends Component {
     super(props)
 
     this.state = {
-      people: []
+      people: [],
+      isLoading: false
     }
   }
 
   componentDidMount () {
+    this.setState({ isLoading: true })
+
     axios
       .get('https://randomuser.me/api/?nat=br&results=150')
       .then(response => {
         const { results } = response.data
-        this.setState({ people: results })
+        this.setState({
+          people: results,
+          isLoading: false
+        })
       })
+  }
+
+  renderLoading () {
+    return <ActivityIndicator size='large' color='#6ca2f7' />
   }
 
   render () {
     return (
       <View>
+        {this.state.isLoading && this.renderLoading()}
         <PeopleList
           people={this.state.people}
           onPressItem={pageParams => {
